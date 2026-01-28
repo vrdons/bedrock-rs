41f5b062c08a640f0b3b4bddacd81d5ad25ad087
vrdons <vrdons@proton.me>
2026-01-28 13:11:05 +0300

    fix linker

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
-adb push target/x86_64-linux-android/debug/bedrock /tmp
-adb shell chmod +x /tmp/bedrock
-adb shell /tmp/bedrock
+waydroid adb connect
+adb push target/x86_64-linux-android/debug/bedrock /data/local/tmp/
+adb shell chmod +x /data/local/tmp/bedrock
+adb shell /data/local/tmp/bedrock
