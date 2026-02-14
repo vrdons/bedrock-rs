52b5f111fd0fbc10ef117469f4582655f734370f
vrdons <vrdons@proton.me>
2026-02-14 17:46:18 +0300

    Update project to 0.2.0

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cargo build --example forwarder --target x86_64-linux-android
-
-waydroid adb connect
-adb push target/x86_64-linux-android/debug/bedrock /data/local/tmp/
-adb shell chmod +x /data/local/tmp/bedrock
-adb shell /data/local/tmp/bedrock
